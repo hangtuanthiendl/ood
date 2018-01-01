28325f3542a7361af555ebcf7b10c69262693e59
huyvu05051996 <huyvu05051996@gmail.com>
2018-01-01 12:34:16 +0700

    Update 1/1/2018
    Update data cho việc test báo cáo thống kê

--- ok.sql
+++ ok.sql
@@ -125,6 +125,8 @@ CREATE TABLE `donthuoc` (
 
 `MAPKB` int(5) unsigned zerofill,
 
+`MANV` int(5) unsigned zerofill NOT NULL,
+
 PRIMARY KEY (`MADT`) 
 
 );
@@ -215,7 +217,17 @@ ALTER TABLE `dichvu` ADD CONSTRAINT `MANV_DGDV` FOREIGN KEY (`MANV`) REFERENCES 
 
 ALTER TABLE `donthuoc` ADD CONSTRAINT `MAPKB_DT` FOREIGN KEY (`MAPKB`) REFERENCES `pkb` (`MAPKB`);
 
+ALTER TABLE `donthuoc` ADD CONSTRAINT `MANV_DT` FOREIGN KEY (`MANV`) REFERENCES `profile` (`MANV`);
 
+
+select hdtk.MAHDTK , hdtk.MAPKB, hdtk.NGAYLAPHD, hdtk.THANHTIEN, profile.TENNV   from `hdtk`, `profile` where NGAYLAPHD >= '2017-05-05' and  NGAYLAPHD <= '2017-08-05' and profile.MANV = hdtk.MANV ;
+
+select donthuoc.MADT , donthuoc.MAPKB, donthuoc.NGAYLAPTT, donthuoc.THANHTIEN, profile.TENNV   from `donthuoc`, `profile` where NGAYLAPTT >= '2017-05-05' and  NGAYLAPTT <= '2017-08-05' and profile.MANV = donthuoc.MANV ;
+
+
+select MONTH(NGAYLAPHD) AS THANG, Sum(THANHTIEN) AS DOANHTHU from `hdtk` where YEAR(NGAYLAPHD) = '2017' group by MONTH(NGAYLAPHD) order by THANG;
+
+select MONTH(NGAYLAPTT) AS THANG, Sum(THANHTIEN) AS DOANHTHU from `donthuoc` where YEAR(NGAYLAPTT) = '2017' group by MONTH(NGAYLAPTT) order by THANG;
 
 
 /* User */
@@ -230,12 +242,40 @@ select * from `profile`;
 update profile set TENNV = "+TENNV +", NGAYSINH = "12/01/1996", GTINH = true, DIACHI = "+DIACHI+", SDT = 01234567, CHUCVU = "+CHUCVU+", enabled = 1,role = "1",temppassword = "+temppassword+" where MANV = "2"
 ;
 
-*/
-SELECT * FROM profile WHERE MANV = 2 and temppassword ='123456';
 
+*/
+select * from donthuoc;
+SELECT * FROM profile WHERE MANV = 2 and temppassword ='123456';
+INSERT INTO `benhvien`.`donthuoc` ( `NGAYLAPTT`, `THANHTIEN`, `MAPKB`, `MANV`) VALUES ('2017-05-15','180000','1', '1');
+INSERT INTO `benhvien`.`donthuoc` ( `NGAYLAPTT`, `THANHTIEN`, `MAPKB`, `MANV`) VALUES ('2017-05-15','170000','2', '1');
+INSERT INTO `benhvien`.`donthuoc` ( `NGAYLAPTT`, `THANHTIEN`, `MAPKB`, `MANV`) VALUES ('2017-05-15','160000','3', '1');
+INSERT INTO `benhvien`.`donthuoc` ( `NGAYLAPTT`, `THANHTIEN`, `MAPKB`, `MANV`) VALUES ('2017-05-15','150000','4', '1');
+INSERT INTO `benhvien`.`donthuoc` ( `NGAYLAPTT`, `THANHTIEN`, `MAPKB`, `MANV`) VALUES ('2017-05-15','140000','5', '1');
+/*insert data bao cao*/
+INSERT INTO `benhvien`.`Benhnhan` (`TENBN`, `NGAYSINH`, `GTINH`, `DCHI`, `SDT`, `CTY`, `TSBENH`, `enabled`) VALUES ('B', '1996-05-20', '1', 'Ho Chi Minh', '123456', 'Ho Chi Minh', 'B', '0');
+INSERT INTO `benhvien`.`Benhnhan` (`TENBN`, `NGAYSINH`, `GTINH`, `DCHI`, `SDT`, `CTY`, `TSBENH`, `enabled`) VALUES ('Nguyễn A', '1996-05-20', '1', 'Ho Chi Minh', '123456', 'Ho Chi Minh', 'B', '0');
+INSERT INTO `benhvien`.`Benhnhan` (`TENBN`, `NGAYSINH`, `GTINH`, `DCHI`, `SDT`, `CTY`, `TSBENH`, `enabled`) VALUES ('Nguyễn B', '1996-05-20', '1', 'Ho Chi Minh', '123456', 'Ho Chi Minh', 'B', '0');
+INSERT INTO `benhvien`.`Benhnhan` (`TENBN`, `NGAYSINH`, `GTINH`, `DCHI`, `SDT`, `CTY`, `TSBENH`, `enabled`) VALUES ('Nguyễn C', '1996-05-20', '1', 'Ho Chi Minh', '123456', 'Ho Chi Minh', 'B', '0');
+INSERT INTO `benhvien`.`Benhnhan` (`TENBN`, `NGAYSINH`, `GTINH`, `DCHI`, `SDT`, `CTY`, `TSBENH`, `enabled`) VALUES ('Nguyễn D', '1996-05-20', '1', 'Ho Chi Minh', '123456', 'Ho Chi Minh', 'B', '0');
+
+INSERT INTO `benhvien`.`pkb` (`MABN`, `MANV`, `MABS`, `TRCHUNG`, `CDOAN`, `NGAYKHAM`, `LHEN`, `GCHU`) VALUES ('1', '1', '2', 'Khùng', 'Sắp chết', '2017-05-05', 'dasdaa', 'asdasdad');
+INSERT INTO `benhvien`.`pkb` (`MABN`, `MANV`, `MABS`, `TRCHUNG`, `CDOAN`, `NGAYKHAM`, `LHEN`, `GCHU`) VALUES ('2', '1', '2', 'Khùng', 'Sắp chết', '2017-05-05', 'dasdaa', 'asdasdad');
+INSERT INTO `benhvien`.`pkb` (`MABN`, `MANV`, `MABS`, `TRCHUNG`, `CDOAN`, `NGAYKHAM`, `LHEN`, `GCHU`) VALUES ('3', '1', '2', 'Khùng', 'Sắp chết', '2017-05-05', 'dasdaa', 'asdasdad');
+INSERT INTO `benhvien`.`pkb` (`MABN`, `MANV`, `MABS`, `TRCHUNG`, `CDOAN`, `NGAYKHAM`, `LHEN`, `GCHU`) VALUES ('4', '1', '2', 'Khùng', 'Sắp chết', '2017-05-05', 'dasdaa', 'asdasdad');
+INSERT INTO `benhvien`.`pkb` (`MABN`, `MANV`, `MABS`, `TRCHUNG`, `CDOAN`, `NGAYKHAM`, `LHEN`, `GCHU`) VALUES ('5', '1', '2', 'Khùng', 'Sắp chết', '2017-05-05', 'dasdaa', 'asdasdad');
+
+INSERT INTO `benhvien`.`hdtk` (`MAPKB`, `MANV`, `NGAYLAPHD`, `THANHTIEN`) VALUES ('1', '1', '2017-05-15', '180000');
+INSERT INTO `benhvien`.`hdtk` (`MAPKB`, `MANV`, `NGAYLAPHD`, `THANHTIEN`) VALUES ('2', '1', '2017-06-05', '160000');
+INSERT INTO `benhvien`.`hdtk` (`MAPKB`, `MANV`, `NGAYLAPHD`, `THANHTIEN`) VALUES ('3', '1', '2017-07-05', '140000');
+INSERT INTO `benhvien`.`hdtk` (`MAPKB`, `MANV`, `NGAYLAPHD`, `THANHTIEN`) VALUES ('4', '1', '2017-08-05', '150000');
+INSERT INTO `benhvien`.`hdtk` (`MAPKB`, `MANV`, `NGAYLAPHD`, `THANHTIEN`) VALUES ('5', '1', '2017-09-05', '170000');
 /*DichVu*/
 
-insert into dichvu(`TENDICHVU`,`MANV`, `GIADICHVU`) values('Khám tổng quát',1,200000);
+insert into dichvu(`TENDICHVU`,`MANV`, `GIADICHVU`, `enabled`) values('Khám tổng quát',1,300000, 1);
+insert into dichvu(`TENDICHVU`,`MANV`, `GIADICHVU`, `enabled`) values('Khám nội soi 1',1,200000, 1);
+insert into dichvu(`TENDICHVU`,`MANV`, `GIADICHVU`, `enabled`) values('Siêu âm lồng ngực',1,400000, 1);
+
+
 insert into dichvu(`TENDICHVU`,`MANV`, `GIADICHVU`) values('Khám nội soi 1',1,300000);
 insert into dichvu(`TENDICHVU`,`MANV`, `GIADICHVU`) values('Siêu âm lồng ngực',2,400000);
 
